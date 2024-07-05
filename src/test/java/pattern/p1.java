1a7acf279ebafc759d9dc4af91bff5ee810ef962
Users <nilurautomationtest@gmail.com>
2024-07-05 16:43:48 +0530

    initial commit

---
+++ src/test/java/pattern/p1.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class p1 {
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++) {
+			for(int j=5-1; j>=i; j--) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print("p");
+			}
+			System.out.println();
+		}
+		System.out.println("=======================");
+		
+	}
+}
